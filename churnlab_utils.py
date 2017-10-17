811808a21fb238a7f9d6c378f3cf8ba6c16f2421
Your Name <you@example.com>
2017-10-17 00:45:14 +0000

    adding pickle files and lab2

--- churnlab_utils.py
+++ churnlab_utils.py
@@ -1,41 +1,28 @@
 import pandas as pd
 from sklearn.cluster import DBSCAN
 from itertools import compress
-class MaskableList(list):
-    def __getitem__(self, index):
-        try: return super(MaskableList, self).__getitem__(index)
-        except TypeError: return MaskableList(compress(self, index))
-        
-def DescribeItemRange(df):  # Show the number of unique values in each column
+def pdutil_describeItemRange(df):  # Show the number of unique values in each column
+    """pdutil_describeItemRange performs is similar to descrive for a DataFrame"""
+    """but it displays the number of unique values in each column"""
+    """argument is a dataFrame """
+    """return is smaller dataFrame with the same columns as the original"""
     cols = df.columns.tolist()
     vals = pd.DataFrame ( [ len(set(df[s])) for s in df.columns.tolist()] ).T
     vals.columns = cols
     return vals
 
-def ReplaceNans (df):  #replace NaNs in all df locations with zeros
-    df.fillna(0)
-    return df
-
-def SelectZeroColumns (df): #return a list with column names of zero filled columns
-    dropcols = (df == 0).all().astype(int).tolist()
-    return [s for s in mylist[dropcols]  ]
-
-def SelectNonZeroColumns(df):  #return a list with columns names of nonzero columns
-    return list(set(StringifyColumnNames(df))- set(SelectZeroColumns(df) ))
-
-def StringifyColumnNames (df):   # handle unicode strings and covert to normal strings
-    cols = df.columns.tolist()
-    cols = [str(r) for r in cols]
-    return cols
-
-def Factorize(df):
+def pdutil_factorize(df):
+    """pdutil_factorize performs converts every categorical column in a dataFrame to numeric values"""
+    """pass in a DataFrame"""
+    """return value is a similar dataFrame but with categorical values converted to numbers"""    
     return  df.apply(lambda x: pd.factorize(x)[0])
 
-def MyOneHotEncode(pdold, StaticCols, EncodeCols):
-    """MyOneHotEncode performs OneHotEncoding on specified columns in our dataframe"""
+def pdutil_OneHotEncode(pdold, StaticCols, EncodeCols):
+    """pdutil_OneHotEncode performs OneHotEncoding on specified columns in our dataframe"""
     """pdold is the old dataframe we are paasing in"""
     """StaticCols is a list of columns we do not wish to encode"""
     """EncodeCols is a list of columns we wish to encode"""
+    """Returns a modified DataFrame with oneHotEncoded columns"""
     # make sure that we our static list is not in our encode list
     cols_OneHotEncode = sorted(list(set(EncodeCols) - set(StaticCols)))
     # create a new dataframe which represents the static columns
@@ -54,7 +41,7 @@ def MyOneHotEncode(pdold, StaticCols, EncodeCols):
     return pdnew
 
 
-def my_dbscan(df, eps_):
+def pdutil_dbscan(df, eps_):
 # Compute DBSCAN
     labels = []
     db = DBSCAN(eps=eps_, min_samples=100).fit(df)
@@ -62,7 +49,6 @@ def my_dbscan(df, eps_):
     #n_clusters_ = labels[0].madf()
     n_clusters_ = len(list(set(db.labels_))) - (1 if -1 in db.labels_ else 0)
     # Number of clusters in labels, ignoring noise if present.
-    #tdft = 'dbsPCA_' + str(n_clusters_)
     tdft = 'dbsPCA'
     df[tdft]=labels
     print('eps:{} Estimated number of clusters: {}'.format(eps_, n_clusters_) ) 
